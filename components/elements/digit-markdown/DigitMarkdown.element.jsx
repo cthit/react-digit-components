9c3c38d0fd7912dafa62e42899626c69a611655d
portals <theodor.ang.98@gmail.com>
2018-09-22 19:27:29 +0200

    Added DigitTable to render Tables in Markdown

--- components/elements/digit-markdown/DigitMarkdown.element.jsx
+++ components/elements/digit-markdown/DigitMarkdown.element.jsx
@@ -1,12 +1,58 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import { DigitText } from "../..";
+import DigitTable from "../../views/digit-table";
 
 const DigitMarkdown = ({ markdownSource }) => (
   <div>
     <ReactMarkdown
       source={markdownSource}
       renderers={{
+        table: data => {
+          const headerTextsArray = data.children[0].props.children[0].props.children.map(
+            cellRawObject => {
+              return cellRawObject.props.children[0];
+            }
+          );
+
+          const headerTexts = {};
+
+          for (let i = 0; i < headerTextsArray.length; i++) {
+            const headerText = headerTextsArray[i];
+            headerTexts[headerText] = headerText;
+          }
+
+          const rowsTexts = data.children[1].props.children.map(
+            rowRawObject => {
+              //Each cell
+              const cells = rowRawObject.props.children.map(cellRawObject => {
+                return cellRawObject.props.children[0];
+              });
+
+              const rowObject = {};
+
+              for (let i = 0; i < cells.length; i++) {
+                const cellText = cells[i];
+                const headerText = headerTextsArray[i];
+                rowObject[headerText] = cellText;
+              }
+
+              return rowObject;
+            }
+          );
+
+          return (
+            <DigitTable
+              titleText=""
+              searchText=""
+              idProp={first}
+              startOrderBy={first}
+              columnsOrder={headerTextsArray}
+              headerTexts={headerTexts}
+              data={rowsTexts}
+            />
+          );
+        },
         heading: data => {
           const text = data.children[0];
           switch (data.level) {

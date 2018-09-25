ab251714c7431607949c2653ceb41f96a96471fc
Elias Sundqvist <elias@sundqvist.com>
2018-09-25 20:36:43 +0200

    Initial commit for feature/grid

---
+++ stories/digit-grid.stories.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+import {
+  withKnobs,
+  select,
+  text,
+  boolean,
+  number
+} from "@storybook/addon-knobs";
+import { storiesOf } from "@storybook/react";
+import { withReadme } from "storybook-readme";
+
+import DigitGridReadme from "../components/styles/digit-layout/grid-readme.md";
+import {
+  Column,
+  Row,
+  Size,
+  Fill,
+  Center,
+  Flex,
+  Grid,
+  GridItem
+} from "../components/styles/digit-layout/DigitLayout.styles";
+import { Display } from "../components/styles/digit-text/DigitText.styles";
+import styled from "styled-components";
+
+const DigitGridStory = storiesOf("Layout", module);
+
+const alignLabel = "Align";
+const alignOptions = ["Left", "Right", "None"];
+const alignDefaultValue = "None";
+
+DigitGridStory.addDecorator(withKnobs);
+
+const BorderSize = styled(Size)`
+  border: 1px solid black;
+`;
+
+const BackgroundColor = styled(Fill)`
+  background-color: ${props => props.color};
+`;
+
+const DummyItem = ({ text, color }) => (
+  <BackgroundColor color={color}>
+    <Center>
+      <Display text={text} />
+    </Center>
+  </BackgroundColor>
+);
+
+DigitGridStory.add(
+  "DigitGrid",
+  withReadme(DigitGridReadme, () => {
+    let numItems = number("num of items", 5);
+    let numCols = number("number of columns", 3);
+
+    return (
+      <BorderSize absWidth="500px" absHeight="500px">
+        <Grid fill columns={`repeat(${numCols}, 1fr)`}>
+          {Array.from(new Array(numItems), (_, i) => (
+            <GridItem>
+              <DummyItem
+                text={`${i + 1}`}
+                color={["red", "green", "blue", "yellow"][i % 4]}
+              >
+                {" "}
+              </DummyItem>
+            </GridItem>
+          ))}
+        </Grid>
+      </BorderSize>
+    );
+  })
+);

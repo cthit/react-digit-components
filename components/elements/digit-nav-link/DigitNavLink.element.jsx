cb752033d255323e6a02fb720ce9b7dd694f2ef0
portals <theodor.ang.98@gmail.com>
2018-10-08 13:12:40 +0200

    Adds proptypes to DigitNavLink

--- components/elements/digit-nav-link/DigitNavLink.element.jsx
+++ components/elements/digit-nav-link/DigitNavLink.element.jsx
@@ -1,22 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 import { ListItemText, ListItem, ListItemIcon } from "@material-ui/core";
 
 const DigitNavLink = ({ text, link, onClick, icon }) => (
   <Link to={link}>
-      <ListItem button>
-          {icon&&
-          <ListItemIcon>
-                {React.createElement(icon)}
-          </ListItemIcon>}
-          <ListItemText onClick={onClick}>{text}</ListItemText>
-      </ListItem>
+    <ListItem button>
+      {icon && <ListItemIcon>{React.createElement(icon)}</ListItemIcon>}
+      <ListItemText onClick={onClick}>{text}</ListItemText>
+    </ListItem>
   </Link>
 );
 
 const Link = styled(NavLink)`
   text-decoration: none;
 `;
+
+DigitNavLink.propTypes = {
+  text: PropTypes.string.isRequired,
+  icon: PropTypes.element,
+  link: PropTypes.string.isRequired,
+  onClick: PropTypes.func
+};
 
 export default DigitNavLink;

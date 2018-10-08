c95e6081006e4c89a17fffafef4905138476fda6
portals <theodor.ang.98@gmail.com>
2018-10-08 14:47:12 +0200

    Adds proptypes to styled components in DigitLayout

--- components/styles/digit-layout/DigitLayout.styles.jsx
+++ components/styles/digit-layout/DigitLayout.styles.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
 
 /**
+ * display: display {"flex" | "inline-flex"}
  * justify-content: justifyContent {"flex-start" | "flex-end" | "center" | "space-between" | "space-around" | "space-evenly"}
  * flex-wrap: flexWrap {"nowrap" | "wrap" | "wrap-reverse"}
  * align-items: alignItems {"flex-start" | "flex-end" | "center" | "baseline" | "stretch"}
@@ -16,6 +18,34 @@ export const Flex = styled.div`
   align-content: ${props =>
     props.alignContent != null ? props.alignContent : ""};
 `;
+
+Flex.propTypes = {
+  display: PropTypes.oneOf(["flex", "inline-flex"]),
+  justifyContent: PropTypes.oneOf([
+    "flex-start",
+    "flex-end",
+    "center",
+    "space-between",
+    "space-around",
+    "space-evenly"
+  ]),
+  flexWrap: PropTypes.oneOf(["nowrap", "wrap", "wrap-reverse"]),
+  alignItems: PropTypes.oneOf([
+    "flex-start",
+    "flex-end",
+    "center",
+    "baseline",
+    "stretch"
+  ]),
+  alignContent: PropTypes.oneOf([
+    "flex-start",
+    "flex-end",
+    "center",
+    "space-between",
+    "space-around",
+    "stretch"
+  ])
+};
 
 function flexAlignLeftOrTop(leftOrTopAlign, rightOrBottomAlign, reverse) {
   if (reverse ? rightOrBottomAlign : leftOrTopAlign) {
@@ -85,6 +115,18 @@ export const Column = styled(
   flex: ${props => (props.fill ? "1" : "")};
 `;
 
+Column.propTypes = {
+  center: PropTypes.bool,
+  centerVertical: PropTypes.bool,
+  centerHorizontal: PropTypes.bool,
+  topAlign: PropTypes.bool,
+  bottomAlign: PropTypes.bool,
+  reverse: PropTypes.bool,
+  padding: PropTypes.string,
+  fill: PropTypes.bool,
+  children: PropTypes.arrayOf(PropTypes.element)
+};
+
 export const Row = styled(
   ({
     center,
@@ -115,6 +157,18 @@ export const Row = styled(
   }
   flex: ${props => (props.fill ? "1" : "")};
 `;
+
+Row.propTypes = {
+  center: PropTypes.bool,
+  centerVertical: PropTypes.bool,
+  centerHorizontal: PropTypes.bool,
+  leftAlign: PropTypes.bool,
+  rightAlign: PropTypes.bool,
+  reverse: PropTypes.bool,
+  padding: PropTypes.string,
+  fill: PropTypes.bool,
+  children: PropTypes.arrayOf(PropTypes.element)
+};
 
 export const DownRightPosition = styled.div`
   position: absolute;
@@ -183,9 +237,17 @@ export const HideFill = styled(Fill)`
   display: ${props => (props.hidden ? "none" : "inherit")};
 `;
 
+HideFill.propTypes = {
+  hidden: PropTypes.bool
+};
+
 export const Hide = styled.div`
   display: ${props => (props.hidden ? "none" : "inherit")};
 `;
+
+Hide.propTypes = {
+  hidden: PropTypes.bool
+};
 
 export const Size = styled(Flex)`
   width: ${props => (props.absWidth != null ? props.absWidth : props.width)};
@@ -202,3 +264,12 @@ export const Size = styled(Flex)`
   min-height: ${props =>
     props.absHeight != null ? props.absHeight : props.minHeight};
 `;
+
+Size.propTypes = {
+  absWidth: PropTypes.string,
+  absHeight: PropTypes.string,
+  minWidth: PropTypes.string,
+  minHeight: PropTypes.string,
+  maxWidth: PropTypes.string,
+  maxHeight: PropTypes.string
+};

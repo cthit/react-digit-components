74de99b72823cdd54f6ea838783867a8c25bb8d9
portals <theodor.ang.98@gmail.com>
2018-10-08 13:51:35 +0200

    Adds proptypes to DigitTooltip

--- components/elements/digit-tooltip/DigitTooltip.element.jsx
+++ components/elements/digit-tooltip/DigitTooltip.element.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 import { Tooltip } from "@material-ui/core";
 import { Fill } from "../../styles/digit-layout/DigitLayout.styles";
 
@@ -8,5 +8,10 @@ const DigitTooltip = ({ children, text }) => (
     <Fill>{children}</Fill>
   </Tooltip>
 );
+
+DigitTooltip.propTypes = {
+  children: PropTypes.element.isRequired,
+  text: PropTypes.text.isRequired
+};
 
 export default DigitTooltip;

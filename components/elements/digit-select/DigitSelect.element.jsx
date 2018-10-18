25a6710a95cff2219891cc4781ad9e4685a24ca8
portals <theodor.ang.98@gmail.com>
2018-10-18 22:09:27 +0200

    Fixes outline for DigitSelect

--- components/elements/digit-select/DigitSelect.element.jsx
+++ components/elements/digit-select/DigitSelect.element.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import {
   Select,
@@ -14,76 +15,79 @@ import styled from "styled-components";
 
 import { Fill } from "../../styles/digit-layout/DigitLayout.styles";
 
-const element = document.createElement("canvas");
-const context = element.getContext("2d");
+class DigitSelect extends React.Component {
+  render() {
+    const {
+      value,
+      onChange,
+      disabled,
+      valueToTextMap,
+      allowToChooseNone,
+      name,
+      upperLabel,
+      lowerLabel,
+      reverse,
+      inputProps,
+      filled,
+      outline
+    } = this.props;
+    return (
+      <Fill>
+        <StyledFormControl
+          variant={filled ? "filled" : outline ? "outlined" : "standard"}
+        >
+          <InputLabel
+            ref={ref => {
+              this.labelRef = ReactDOM.findDOMNode(ref);
+            }}
+          >
+            {upperLabel}
+          </InputLabel>
+          <StyledSelect
+            onChange={onChange}
+            disabled={disabled}
+            displayEmpty={allowToChooseNone}
+            value={value}
+            variant={filled ? "filled" : outline ? "outlined" : "standard"}
+            input={
+              filled ? (
+                <FilledInput />
+              ) : outline ? (
+                <OutlinedInput
+                  labelWidth={this.labelRef ? this.labelRef.offsetWidth : 0}
+                />
+              ) : (
+                <Input />
+              )
+            }
+            inputProps={{
+              id: "id-" + name,
+              name: name,
+              color: "secondary",
+              ...inputProps
+            }}
+          >
+            {allowToChooseNone ? (
+              <MenuItem value="" name="Nothing">
+                {""}
+              </MenuItem>
+            ) : null}
 
-function get_tex_size(txt, font) {
-  context.font = font;
-  return context.measureText(txt).width;
+            {_getValues(valueToTextMap, reverse).map(value => {
+              const text = valueToTextMap[value];
+              return (
+                <MenuItem name={value} key={value} value={value}>
+                  {text}
+                </MenuItem>
+              );
+            })}
+          </StyledSelect>
+          <FormHelperText>{lowerLabel}</FormHelperText>
+        </StyledFormControl>
+      </Fill>
+    );
+  }
 }
-
-const DigitSelect = ({
-  value,
-  onChange,
-  disabled,
-  valueToTextMap,
-  allowToChooseNone,
-  name,
-  upperLabel,
-  lowerLabel,
-  reverse,
-  inputProps,
-  filled,
-  outline
-}) => (
-  <Fill>
-    <StyledFormControl
-      variant={filled ? "filled" : outline ? "outlined" : "standard"}
-    >
-      <InputLabel>{upperLabel}</InputLabel>
-      <StyledSelect
-        onChange={onChange}
-        disabled={disabled}
-        displayEmpty={allowToChooseNone}
-        value={value}
-        variant={filled ? "filled" : outline ? "outlined" : "standard"}
-        input={
-          filled ? (
-            <FilledInput />
-          ) : outline ? (
-            <OutlinedInput
-              labelWidth={get_tex_size(upperLabel, "1rem Helvetica") * 0.75 + 8}
-            />
-          ) : (
-            <Input />
-          )
-        }
-        inputProps={{
-          id: "id-" + name,
-          name: name,
-          color: "secondary",
-          ...inputProps
-        }}
-      >
-        {allowToChooseNone ? (
-          <MenuItem value="" name="Nothing">
-            {""}
-          </MenuItem>
-        ) : null}
-
-        {_getValues(valueToTextMap, reverse).map(value => {
-          const text = valueToTextMap[value];
-          return (
-            <MenuItem name={value} key={value} value={value}>
-              {text}
-            </MenuItem>
-          );
-        })}
-      </StyledSelect>
-      <FormHelperText>{lowerLabel}</FormHelperText>
-    </StyledFormControl>
-  </Fill>
-);
 
 function _getValues(valueToTextMap, reverse) {
   var result = Object.keys(valueToTextMap);

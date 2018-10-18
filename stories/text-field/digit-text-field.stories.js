6821f6eb23b58a54f1693f771df49cc08f3d23a5
portals <theodor.ang.98@gmail.com>
2018-10-18 20:35:34 +0200

    Removes react-powerplug from DigiTextField

--- stories/text-field/digit-text-field.stories.js
+++ stories/text-field/digit-text-field.stories.js
@@ -1,15 +1,12 @@
 import React from "react";
 
 import { withKnobs, select, text, boolean } from "@storybook/addon-knobs";
-import { withInfo } from "@storybook/addon-info";
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
-import { State } from "react-powerplug";
 import { withReadme } from "storybook-readme";
 
-import { DigitTextField, DigitLayout, DigitProviders } from "../../components";
+import { DigitLayout, DigitProviders } from "../../components";
 import DigitTextFieldReadme from "../../components/elements/digit-text-field/readme.md";
+import StoryDigitTextField from "./StoryDigitTextField";
 
 const typeLabel = "type";
 const typeOptions = ["normal", "password", "numbersOnly"];
@@ -36,33 +33,17 @@ DigitTextFieldStory.add(
 
     return (
       <DigitProviders>
-        <State initial={{ value: "This is text" }}>
-          {({ state, setState }) => (
-            <DigitLayout.Size width="300px">
-              <DigitTextField
-                value={state.value}
-                onChange={e => {
-                  setState({
-                    value: e.target.value
-                  });
-                  action("value_changed")(e);
-                }}
-                onBlur={e => {
-                  action("blur")(e);
-                }}
-                password={type === "password"}
-                numbersOnly={type === "numbersOnly"}
-                error={error}
-                errorMessage={errorMessage}
-                lowerLabel={lowerLabel}
-                upperLabel={upperLabel}
-                disabled={disabled}
-                outline={style === "outline"}
-                filled={style === "filled"}
-              />
-            </DigitLayout.Size>
-          )}
-        </State>
+        <DigitLayout.Size width="300px">
+          <StoryDigitTextField
+            type={type}
+            upperLabel={upperLabel}
+            lowerLabel={lowerLabel}
+            error={error}
+            errorMessage={errorMessage}
+            disabled={disabled}
+            style={style}
+          />
+        </DigitLayout.Size>
       </DigitProviders>
     );
   })

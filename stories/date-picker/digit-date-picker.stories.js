049d7289dc55e7e67f3d08b07479b7c9207227ab
portals <theodor.ang.98@gmail.com>
2018-10-18 15:48:08 +0200

    Removes react-powerplug from DigitDatePicker story

--- stories/date-picker/digit-date-picker.stories.js
+++ stories/date-picker/digit-date-picker.stories.js
@@ -1,15 +1,12 @@
 import React from "react";
 
-import { withKnobs, select, text, boolean } from "@storybook/addon-knobs";
+import { withKnobs, text } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
 import { withReadme } from "storybook-readme";
 
 import { DigitProviders, DigitLayout } from "../../components";
 import DigitDatePickerReadme from "../../components/elements/digit-date-picker/readme.md";
-import DigitDatePicker from "../../components/elements/digit-date-picker/DigitDatePicker.element";
-import { Value } from "react-powerplug";
+import StoryDigitDatePicker from "./StoryDigitDatePicker";
 
 const DigitDatePickerStory = storiesOf("Elements", module);
 
@@ -22,20 +19,9 @@ DigitDatePickerStory.add(
 
     return (
       <DigitProviders>
-        <Value
-          initial={new Date()}
-          render={({ set, value }) => (
-            <DigitLayout.Size absWidth="300px" absHeight="300px">
-              <DigitDatePicker
-                upperLabel={upperLabel}
-                value={value}
-                onChange={date => {
-                  set(date);
-                }}
-              />
-            </DigitLayout.Size>
-          )}
-        />
+        <DigitLayout.Size absWidth="300px" absHeight="300px">
+          <StoryDigitDatePicker upperLabel={upperLabel} />
+        </DigitLayout.Size>
       </DigitProviders>
     );
   })

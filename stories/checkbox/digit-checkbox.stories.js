0af8ff32270004bf1031d7081f41c6576cd9c7d1
portals <theodor.ang.98@gmail.com>
2018-10-18 15:40:52 +0200

    Removes react-powerplug from DigitDateAndTimePicker story

--- stories/checkbox/digit-checkbox.stories.js
+++ stories/checkbox/digit-checkbox.stories.js
@@ -3,7 +3,6 @@ import React from "react";
 import { withKnobs, select, text, boolean } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import { State } from "react-powerplug";
 import { withReadme } from "storybook-readme";
 
 import { DigitProviders } from "../../components";

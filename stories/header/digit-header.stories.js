0c3f5eefed40903e023004342f510bfdcadcd4cf
portals <theodor.ang.98@gmail.com>
2018-10-18 18:44:04 +0200

    Removes react-renderplug from DigitHeader story

--- stories/header/digit-header.stories.js
+++ stories/header/digit-header.stories.js
@@ -8,23 +8,12 @@ import {
   number
 } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
 import { withReadme } from "storybook-readme";
 
-import { Send, Info, Code } from "@material-ui/icons";
-
-import {
-  DigitHeader,
-  DigitNavLink,
-  DigitProviders,
-  DigitLayout,
-  DigitDisplayData,
-  DigitTextField,
-  DigitButton
-} from "../../components";
+import { DigitProviders } from "../../components";
 import DigitHeaderReadme from "../../components/elements/digit-header/readme.md";
-import { Value } from "react-powerplug";
+
+import StoryDigitHeader from "./StoryDigitHeader";
 
 const iconLabel = "Icon";
 const iconOptions = ["Send", "Info", "Code"];
@@ -50,68 +39,12 @@ DigitHeaderStory.add(
 
     return (
       <DigitProviders>
-        <DigitHeader
+        <StoryDigitHeader
           title={title}
-          headerHeight={headerHeight + "px"}
-          renderHeader={() =>
-            customHeaderDemo && (
-              <DigitLayout.Row>
-                <Value
-                  render={({ value, set }) => (
-                    <DigitTextField
-                      upperLabel="Upper upper label"
-                      lowerLabel="Lower lower label"
-                      value={value}
-                      onChange={e => {
-                        set(e.target.value);
-                      }}
-                    />
-                  )}
-                />
-                <DigitButton text="Logga in" />
-              </DigitLayout.Row>
-            )
-          }
-          renderMain={() => (
-            <DigitLayout.Center>
-              <DigitDisplayData
-                fixedWidth={"200px"}
-                data={{
-                  firstName: "Asdf",
-                  lastName: "Asdfsson",
-                  email: "email",
-                  nick: "Asdfasdf"
-                }}
-                keysText={{
-                  firstName: "Förnamn",
-                  lastName: "Efternamn",
-                  email: "Email",
-                  nick: "Nick"
-                }}
-                keysOrder={["firstName", "lastName", "email", "nick"]}
-              />
-            </DigitLayout.Center>
-          )}
-          renderDrawer={
-            navigation
-              ? closeDrawer => (
-                  <DigitNavLink
-                    onClick={closeDrawer}
-                    link="/hej"
-                    text="Hej"
-                    icon={
-                      icon === "Send"
-                        ? Send
-                        : icon === "Info"
-                          ? Info
-                          : icon === "Code"
-                            ? Code
-                            : null
-                    }
-                  />
-                )
-              : null
-          }
+          icon={icon}
+          navigation={navigation}
+          customHeaderDemo={customHeaderDemo}
+          headerHeight={headerHeight}
         />
       </DigitProviders>
     );

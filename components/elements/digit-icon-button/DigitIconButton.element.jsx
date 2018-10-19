a7687e9199342cbf5befd9e8c670a9df69f611fc
portals <theodor.ang.98@gmail.com>
2018-10-19 20:02:39 +0200

    Adds proptypes documentation to DigitIconButton

--- components/elements/digit-icon-button/DigitIconButton.element.jsx
+++ components/elements/digit-icon-button/DigitIconButton.element.jsx
@@ -22,10 +22,17 @@ const DigitIconButton = ({
 
 DigitIconButton.displayName = "DigitIconButton";
 DigitIconButton.propTypes = {
+  /** The function which will be called when the button is clicked. */
   onClick: PropTypes.func.isRequired,
+  /** Sets the color to the primary one. This has predence precedence over secondary. */
   primary: PropTypes.bool,
+  /** Sets the color to the secondary one. This has precedence over the normal button. */
   secondary: PropTypes.bool,
+  /** Disables the button. onClick will not be called if you click the button.
+   * The styling of the button also changes to reflect.
+   */
   disabled: PropTypes.bool,
+  /** The icon of the FAB. Use @material-ui/icons.  */
   icon: PropTypes.func.isRequired
 };
 

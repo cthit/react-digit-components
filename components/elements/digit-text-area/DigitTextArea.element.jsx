7adc238510b33e37808fcc69f41778aed728404f
portals <theodor.ang.98@gmail.com>
2018-10-19 21:11:24 +0200

    Adds proptypes documentation to DigitTextAreaa

--- components/elements/digit-text-area/DigitTextArea.element.jsx
+++ components/elements/digit-text-area/DigitTextArea.element.jsx
@@ -109,23 +109,46 @@ class DigitTextArea extends React.Component {
 
 DigitTextArea.displayName = "DigitTextArea";
 DigitTextArea.propTypes = {
+  /** The value of the text area. Note that this
+   * element is uncontrolled, meaning you have to store
+   * the value of the text area yourself. For updates of the value,
+   * use the onChange function.
+   */
   value: PropTypes.oneOfType([
     PropTypes.string.isRequired,
     PropTypes.number.isRequired
   ]),
+  /** This function will be called when the input changes.
+   * The first argument is the event. To get the new value,
+   * use e.target.value.
+   */
   onChange: PropTypes.func.isRequired,
+  /** The onBlur event occurs when DigitTextArea loses focus. */
   onBlur: PropTypes.func,
+  /** The text label over the DigitSelect */
   upperLabel: PropTypes.string,
+  /** The text label under the DigitSelect */
   lowerLabel: PropTypes.string,
+  /** A unique name relative to a form. e.g. pizzaTopping or attendanceYear.*/
   name: PropTypes.string,
-  password: PropTypes.bool,
-  numbersOnly: PropTypes.bool,
+  /** If true, then errorMessage will be shown instead of lowerLabel */
   error: PropTypes.bool,
+  /** If error is true, then this errorMessage will be shown instead of lowerLabel */
   errorMessage: PropTypes.string,
+  /** If true, then you can't edit this text area. */
   disabled: PropTypes.bool,
+  /** The least amount of rows for this text area. */
   rows: PropTypes.number.isRequired,
+  /** The max amount of rows for this text area. After
+   * the text area has hit this amount of rows, it will begin to
+   * scroll instead of expanding.
+   */
   rowsMax: PropTypes.number.isRequired,
+  /**
+   * Adds an outline around the button in black color.
+   */
   outlined: PropTypes.bool,
+  /** Adds a grey isch background */
   filled: PropTypes.bool
 };
 

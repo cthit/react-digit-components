4c4b1357ac95fda69f53603989a34e6052fe5cde
portals <theodor.ang.98@gmail.com>
2018-10-19 21:16:54 +0200

    Adds proptypes documentation to DigitTextArea, also fixes typo in DigitTextArea

--- components/elements/digit-text-field/DigitTextField.element.jsx
+++ components/elements/digit-text-field/DigitTextField.element.jsx
@@ -104,21 +104,43 @@ class DigitTextField extends React.Component {
 
 DigitTextField.displayName = "DigitTextField";
 DigitTextField.propTypes = {
+  /** The value of the text field. Note that this
+   * element is uncontrolled, meaning you have to store
+   * the value of the text field yourself. For updates of the value,
+   * use the onChange function.
+   */
   value: PropTypes.oneOfType([
     PropTypes.string.isRequired,
     PropTypes.number.isRequired
   ]),
+  /** This function will be called when the input changes.
+   * The first argument is the event. To get the new value,
+   * use e.target.value.
+   */
   onChange: PropTypes.func.isRequired,
+  /** The onBlur event occurs when DigitTextArea loses focus. */
   onBlur: PropTypes.func,
+  /** The text label over the DigitSelect */
   upperLabel: PropTypes.string,
+  /** The text label under the DigitSelect */
   lowerLabel: PropTypes.string,
+  /** A unique name relative to a form. e.g. pizzaTopping or attendanceYear.*/
   name: PropTypes.string,
+  /** If true, then every character will be replaced by a dot. */
   password: PropTypes.bool,
+  /** If true, then only number are allowed. */
   numbersOnly: PropTypes.bool,
+  /** If true, then errorMessage will be shown instead of lowerLabel */
   error: PropTypes.bool,
+  /** If error is true, then this errorMessage will be shown instead of lowerLabel */
   errorMessage: PropTypes.string,
+  /** If true, then you can't edit this text field. */
   disabled: PropTypes.bool,
+  /**
+   * Adds an outline around the button in black color.
+   */
   outlined: PropTypes.bool,
+  /** Adds a grey isch background */
   filled: PropTypes.bool
 };
 

9d560e8d1b1a699aa5b0c1455c32a044def6cabe
portals <theodor.ang.98@gmail.com>
2018-10-19 18:13:58 +0200

    Adds proptypes documentation to DigitDateAndTimePicker

--- components/elements/digit-date-and-time-picker/DigitDateAndTimePicker.element.jsx
+++ components/elements/digit-date-and-time-picker/DigitDateAndTimePicker.element.jsx
@@ -59,19 +59,43 @@ const DigitDateAndTimePicker = ({
 
 DigitDateAndTimePicker.displayName = "DigitDateAndTimePicker";
 DigitDateAndTimePicker.propTypes = {
+  /** If true, then a button will exists to let the user clear the date. */
   clearable: PropTypes.bool,
+  /** If true, then the user can't select a date past now. */
   disablePast: PropTypes.bool,
+  /** What will be presented if a date is invalid */
   invalidLabel: PropTypes.string,
+  /** What will be presented if no date has been selecteed */
   emptyLabel: PropTypes.string,
+  /** Button text for the clear button, which will clear the button.
+   * The clear button will only be shown if clearable is true.
+   */
   clearLabel: PropTypes.string,
+  /** The label for the ok button. The user presses this button
+   * when they have selected a date and a time.
+   */
   okLabel: PropTypes.string,
+  /** The label for the cancel button. The user presses button
+   * when they want to close the dialog and not save the date and time.
+   */
   cancelLabel: PropTypes.string,
+  /** A button to select todays date and time. Is only shown
+   * if showTodayButton is true.
+   */
   todayLabel: PropTypes.string,
+  /** The current value. This component is unmanaged, meaning that
+   * you have to store the value and react on changes using
+   * the onChange function.
+   */
   value: PropTypes.instanceOf(Date),
+  /** This function will be called when the user has selected
+   * a new date and a new time.
+   */
   onChange: PropTypes.func.isRequired,
-  label: PropTypes.string,
-  showTodayButton: PropTypes.bool,
-  setFieldValue: PropTypes.func
+  /** A label that is shown over the input field. */
+  upperLabel: PropTypes.string,
+  /** If true, then a today button will be shown.  */
+  showTodayButton: PropTypes.bool
 };
 
 /** This is a temp solution to get the correct prop types from StoryBook. */

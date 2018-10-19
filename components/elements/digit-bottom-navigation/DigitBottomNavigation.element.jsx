8228acd0e575675f9a16137f5d555309396aabd6
portals <theodor.ang.98@gmail.com>
2018-10-19 15:56:50 +0200

    Adds proptypes documentation to DigitBottomNavigation

--- components/elements/digit-bottom-navigation/DigitBottomNavigation.element.jsx
+++ components/elements/digit-bottom-navigation/DigitBottomNavigation.element.jsx
@@ -32,11 +32,26 @@ const DigitBottomNavigation = ({
 
 DigitBottomNavigation.displayName = "DigitBottomNavigation";
 DigitBottomNavigation.propTypes = {
+  /** The selected tab, can be 0 -> (number of tabs - 1).
+   * This is an element, which means its unmanaged.
+   * You need to save selected your self and with
+   * onChange update it.
+   */
   selected: PropTypes.number.isRequired,
+  /** If you want the non selected tabs to show a label. */
   showLabels: PropTypes.bool,
+  /** An array of labels for the tabs. Needs to have the same length as icons*/
   labels: PropTypes.arrayOf(PropTypes.string).isRequired,
+  /** An array of images (use @material-ui/icons). Needs to have the same length as labels */
   icons: PropTypes.arrayOf(PropTypes.element).isRequired,
+  /** A function with the new selected index as the only argument.
+   * Use this to keep track of the currently selected tab.
+   */
   onChange: PropTypes.func.isRequired
+};
+
+DigitBottomNavigation.defaultProps = {
+  showLabels: true
 };
 
 export default DigitBottomNavigation;

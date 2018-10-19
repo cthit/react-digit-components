5cda83b83a809f9e8b10003326c0f59f9695f9c4
portals <theodor.ang.98@gmail.com>
2018-10-19 19:13:28 +0200

    Adds proptypes documentation to DigitFormField

--- components/elements/digit-form-field/DigitFormField.element.jsx
+++ components/elements/digit-form-field/DigitFormField.element.jsx
@@ -23,8 +23,18 @@ const DigitFormField = ({ name, component, componentProps }) => (
 
 DigitFormField.displayName = "DigitFormField";
 DigitFormField.propTypes = {
+  /** A unique name for the form. This name is the key of
+   * the values object in DigitForm.
+   */
   name: PropTypes.string.isRequired,
+  /** The component that the DigitFormField should wrap. Can be for example a
+   * DigitTextField, DigitSwitch, DigitCheckbox or DigitSelect. Anything that is
+   * input.
+   */
   component: PropTypes.func.isRequired,
+  /** Props for the component. E.g. setting upperLabel in DigitTextField or
+   * primary in DigitCheckbox.
+   */
   componentProps: PropTypes.object
 };
 

2988bb8d3f3698a381f9fb443ab4c99394ea38ce
portals <theodor.ang.98@gmail.com>
2018-10-19 17:41:16 +0200

    Adds proptypes documentation to DigitCheckbox

--- components/elements/digit-checkbox/DigitCheckbox.element.jsx
+++ components/elements/digit-checkbox/DigitCheckbox.element.jsx
@@ -34,15 +34,34 @@ const DigitCheckbox = ({
 
 DigitCheckbox.displayName = "DigitCheckbox";
 DigitCheckbox.propTypes = {
+  /** A unique name relative to a form. e.g. acceptedTerms or pizza.*/
   name: PropTypes.string,
+  /** Fires on input change. Note that this component is unmanaged, and that
+   * you have to save value by yourself. Only argument is the event object. If you want
+   * the new text value, use `e.target.value`.
+   */
   onChange: PropTypes.func.isRequired,
+  /** The onblur event occurs when an object loses focus. */
+
   onBlur: PropTypes.func,
+  /** A label that is to the right of the checkbox. */
   label: PropTypes.string,
+  /** The checked value of the checkbox. Note that this component is unmanaged, and that you have to
+   * save your value of yourself.
+   */
   value: PropTypes.bool.isRequired,
+
+  /** Sets the color to the primary one. This has predence precedence over secondary. */
   primary: PropTypes.bool,
+  /** Sets the color to the secondary one. This has precedence over the normal button. */
   secondary: PropTypes.bool,
+  /** If true, then the user can't changed the value of the checkbox.
+   * The value is persisted though when disabled.
+   */
   disabled: PropTypes.bool,
+  /** If true, then the text will be red and errorMessage will be replaced with errorMessage */
   error: PropTypes.bool,
+  /** Will be shown instead of label if error is true. */
   errorMessage: PropTypes.string
 };
 

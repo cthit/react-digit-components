1338ca4973375a5112daf6a1a0ed4de02e84006e
portals <theodor.ang.98@gmail.com>
2018-10-19 21:02:51 +0200

    Adds proptypes documentation to DigitTabs

--- components/elements/digit-tabs/DigitTabs.element.jsx
+++ components/elements/digit-tabs/DigitTabs.element.jsx
@@ -36,11 +36,19 @@ const DigitTabs = ({
 
 DigitTabs.displayName = "DigitTabs";
 DigitTabs.propTypes = {
+  /** The selected tab index */
   selected: PropTypes.number.isRequired,
+  /** An array of labels for the tabs.*/
   labels: PropTypes.arrayOf(PropTypes.string).isRequired,
+  /** If true, then centers the tabs*/
   centered: PropTypes.bool.isRequired,
+  /** If true, then expands the tabs*/
   fullWidth: PropTypes.bool.isRequired,
+  /** A function with the new selected index as the only argument.
+   * Use this to keep track of the currently selected tab.
+   */
   onChange: PropTypes.func.isRequired,
+  /** If true, then the text uses the DigitText.Title font instead */
   titleFont: PropTypes.bool
 };
 

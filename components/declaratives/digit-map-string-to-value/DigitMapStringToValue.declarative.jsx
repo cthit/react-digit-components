d4d50bc16518208f9996ea0375fa40fce6797789
portals <theodor.ang.98@gmail.com>
2019-03-17 13:05:54 +0100

    Resolves #137

--- components/declaratives/digit-map-string-to-value/DigitMapStringToValue.declarative.jsx
+++ components/declaratives/digit-map-string-to-value/DigitMapStringToValue.declarative.jsx
@@ -1,16 +1,16 @@
 import PropTypes from "prop-types";
 
-const DigitMapStringToValue = ({ currentString, stringtoValueMap, render }) =>
-    render(getCurrentValue(currentString, stringtoValueMap));
+const DigitMapStringToValue = ({ currentString, stringToValueMap, render }) =>
+    render(getCurrentValue(currentString, stringToValueMap));
 
-function getCurrentValue(currentString, stringtoValueMap) {
-    if (!stringtoValueMap.hasOwnProperty(currentString)) {
+function getCurrentValue(currentString, stringToValueMap) {
+    if (!stringToValueMap.hasOwnProperty(currentString)) {
         console.log(
             "WARNING: There isn't a Value for the string: " + currentString
         );
         return 0;
     } else {
-        return stringtoValueMap[currentString];
+        return stringToValueMap[currentString];
     }
 }
 
@@ -19,9 +19,15 @@ DigitMapStringToValue.propTypes = {
     /** The current key for the map */
     currentString: PropTypes.string.isRequired,
     /** String to any map */
-    stringtoValueMap: PropTypes.objectOf(PropTypes.any).isRequired,
+    stringToValueMap: PropTypes.objectOf(PropTypes.any).isRequired,
     /** Render function that has the current value of the selected key as the first and only argument */
     render: PropTypes.func.isRequired
+};
+
+DigitMapStringToValue.defaultProps = {
+    stringToValueMap: {},
+    render: () => null,
+    currentString: ""
 };
 
 export default DigitMapStringToValue;

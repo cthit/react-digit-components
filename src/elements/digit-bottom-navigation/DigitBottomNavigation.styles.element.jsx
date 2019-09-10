5cf64417079fd7a3d4998e258edfdb38dfa38bc7
Theodor Angergård <portalprogramming@gmail.com>
2019-09-10 22:31:00 +0200

    Creates a mdx story for digit-bottom-navigation

--- src/elements/digit-bottom-navigation/DigitBottomNavigation.styles.element.jsx
+++ src/elements/digit-bottom-navigation/DigitBottomNavigation.styles.element.jsx
@@ -2,7 +2,7 @@ import BottomNavigation from "@material-ui/core/BottomNavigation";
 import styled from "styled-components";
 
 export const StyledBottomNavigation = styled(BottomNavigation)`
-    position: fixed;
+    position: absolute;
     height: 64px;
     width: 100%;
     bottom: 0px;

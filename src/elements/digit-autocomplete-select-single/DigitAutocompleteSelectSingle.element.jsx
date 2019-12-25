a704fab2aa633e48493b851938e39f745b90ae58
Theodor Angergård <portalprogramming@gmail.com>
2019-12-26 00:13:41 +0100

    Single autocomplete finished #273

--- src/elements/digit-autocomplete-select-single/DigitAutocompleteSelectSingle.element.jsx
+++ src/elements/digit-autocomplete-select-single/DigitAutocompleteSelectSingle.element.jsx
@@ -1,217 +1,79 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import Select from "react-select";
-import useTheme from "@material-ui/styles/useTheme";
-import withStyles from "@material-ui/styles/withStyles";
+import React from "react";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 import TextField from "@material-ui/core/TextField";
-import Paper from "@material-ui/core/Paper";
-import MenuItem from "@material-ui/core/MenuItem";
-import CancelIcon from "@material-ui/icons/Cancel";
-import DigitChip from "../digit-chip";
-import { Text } from "../../styles/digit-text/DigitText.styles";
 import find from "lodash/find";
-
-const styles = {
-    container: {
-        display: "flex",
-        height: 250
-    },
-    input: {
-        display: "flex",
-        flex: "1 1 auto",
-        height: "100%",
-        minWidth: "250px"
-    },
-    valueContainer: {
-        display: "flex",
-        flexWrap: "wrap",
-        flex: 1,
-        alignItems: "center",
-        overflow: "hidden"
-    },
-    noOptionsMessage: {
-        padding: `4px 4px`
-    },
-    paper: {
-        position: "absolute",
-        zIndex: 1000,
-        top: "66%",
-        bottom: "auto"
-    }
-};
-
-function NoOptionsMessage(props) {
-    return <Text text={props.children} />;
-}
-
-function inputComponent({ inputRef, ...props }) {
-    return <div ref={inputRef} {...props} />;
-}
-
-function Control(props) {
-    return (
-        <TextField
-            fullWidth
-            InputProps={{
-                inputComponent,
-                inputProps: {
-                    className: props.selectProps.classes.input,
-                    inputRef: props.innerRef,
-                    children: props.children,
-                    ...props.innerProps
-                }
-            }}
-            {...props.selectProps.textFieldProps}
-            placeholder=""
-        />
-    );
-}
-
-function Option(props) {
-    return (
-        <MenuItem
-            buttonRef={props.innerRef}
-            selected={props.isFocused}
-            component="div"
-            style={{
-                fontWeight: props.isSelected ? 500 : 400
-            }}
-            {...props.innerProps}
-        >
-            {props.children}
-        </MenuItem>
-    );
-}
-
-function SingleValue(props) {
-    return <Text text={props.children} />;
-}
-
-function ValueContainer(props) {
-    return (
-        <div className={props.selectProps.classes.valueContainer}>
-            {props.children}
-        </div>
-    );
-}
-
-function MultiValue(props) {
-    return (
-        <DigitChip
-            label={props.children}
-            onDelete={!props.isDisabled ? props.removeProps.onClick : null}
-            deleteIcon={<CancelIcon {...props.removeProps} />}
-        />
-    );
-}
-
-function Menu(props) {
-    return (
-        <Paper
-            square
-            className={props.selectProps.classes.paper}
-            {...props.innerProps}
-        >
-            {props.children}
-        </Paper>
-    );
-}
-
-const components = {
-    Control,
-    Menu,
-    MultiValue,
-    NoOptionsMessage,
-    Option,
-    SingleValue,
-    ValueContainer
-};
+import translations from "./DigitAutocompleteSelectSingle.element.translations";
+import useDigitTranslations from "../../hooks/use-digit-translations";
 
 const DigitAutocompleteSelectSingle = ({
-    classes,
+    options,
     value,
     onChange,
+    noOptionsText,
+    onBlur,
     upperLabel,
     lowerLabel,
+    name,
     error,
     errorMessage,
-    name,
-    selectableValues,
     disabled,
-    filled,
-    outlined
+    outlined,
+    filled
 }) => {
-    const [state, setState] = useState({
-        menuIsOpen: false
-    });
-    const theme = useTheme();
-
-    function onMenuIsOpenChange(open) {
-        setState({
-            menuIsOpen: open
-        });
-    }
-
-    const { menuIsOpen } = state;
-
-    const selectStyles = {
-        container: () => ({
-            display: "flex",
-            flex: 1
-        }),
-        input: base => ({
-            ...base,
-            color: theme.palette.text.primary
-        })
-    };
-
-    const selectedValue = find(selectableValues, { value });
-    var selectedValueLabel = null;
-
-    if (selectedValue != null) {
-        selectedValueLabel = selectedValue.label;
-    }
+    const [text] = useDigitTranslations(translations);
 
     return (
-        <Select
-            name={name}
-            classes={classes}
-            styles={selectStyles}
-            options={selectableValues}
-            components={components}
-            placeholder=""
-            value={
-                selectedValueLabel == null
-                    ? null
-                    : { value, label: selectedValueLabel }
+        <Autocomplete
+            autoHighlight
+            value={value}
+            onInputChange={(event, value, reason) => {
+                if (reason === "clear") {
+                    onChange({ target: { value: "" } });
+                } else {
+                    const obj = find(options, { text: value });
+                    if (obj != null) {
+                        onChange({ target: { value: obj.value } });
+                    }
+                }
+            }}
+            getOptionDisabled={value => {
+                const obj = find(options, { value });
+                return obj == null || obj.disabled;
+            }}
+            options={options.map(option => option.value)}
+            getOptionLabel={value => {
+                const obj = find(options, { value });
+                if (obj == null) {
+                    return "";
+                } else {
+                    return obj.text;
+                }
+            }}
+            noOptionsText={
+                noOptionsText == null ? text.NoOptions : noOptionsText
             }
-            onChange={e => {
-                onChange({ target: { value: e.value } });
-            }}
-            menuIsOpen={menuIsOpen}
-            onMenuOpen={() => {
-                onMenuIsOpenChange(true);
-            }}
-            onMenuClose={() => {
-                onMenuIsOpenChange(false);
-            }}
-            textFieldProps={{
-                label: upperLabel,
-                error: error,
-                disabled: disabled,
-                helperText:
-                    error && errorMessage != null ? errorMessage : lowerLabel,
-                InputLabelProps: {
-                    shrink: value !== "" || state.singleOpen
-                },
-                variant: outlined ? "outlined" : filled ? "filled" : "standard"
-            }}
+            disabled={disabled}
+            renderInput={params => (
+                <TextField
+                    {...params}
+                    fullWidth
+                    name={name}
+                    error={error}
+                    label={upperLabel}
+                    variant={
+                        outlined ? "outlined" : filled ? "filled" : "standard"
+                    }
+                    helperText={
+                        error && errorMessage != null
+                            ? errorMessage
+                            : lowerLabel != null
+                            ? lowerLabel
+                            : ""
+                    }
+                />
+            )}
         />
     );
 };
 
-DigitAutocompleteSelectSingle.propTypes = {
-    classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(DigitAutocompleteSelectSingle);
+export default DigitAutocompleteSelectSingle;

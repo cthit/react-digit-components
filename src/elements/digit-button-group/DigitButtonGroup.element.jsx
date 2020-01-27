6c61a63efb54bd307c89aaefebc0cace02c9b2e2
Hoidi <tobiakar@student.chalmers.se>
2020-01-27 22:38:41 +0100

    More examples for DigitButtonGroup

--- src/elements/digit-button-group/DigitButtonGroup.element.jsx
+++ src/elements/digit-button-group/DigitButtonGroup.element.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
+import PropTypes from "prop-types";
 
 const DigitButtonGroup = ({
     buttons,
@@ -31,8 +32,38 @@ const DigitButtonGroup = ({
     </ButtonGroup>
 );
 
+
+DigitButtonGroup.displayName = "DigitButtonGroup";
+DigitButtonGroup.propTypes = {
+    /** Array of buttons to be in the ButtonGroup. */
+    buttons: PropTypes.array,
+    /** Sets the color to the primary one. This has predence precedence over secondary. */
+    primary: PropTypes.bool,
+    /** Sets the color to the secondary one. This has precedence over the normal button. */
+    secondary: PropTypes.bool,
+    /** Makes the primary color, secondary or gray the background color. 
+     * Also gives it a "depth" feeling.
+     * */
+    raised: PropTypes.bool,
+    /** Adds an outline around the buttons. */
+    outlined: PropTypes.bool,
+    /** Disables the buttons. */
+    disabled: PropTypes.bool,
+    /** If true, then the buttons will be slightly samller. */
+    small: PropTypes.bool,
+    /** If true, then the buttons will be slightly larger. */
+    large: PropTypes.bool
+};
+
 DigitButtonGroup.defaultProps = {
-    buttons: []
+    buttons: [],
+    primary: false,
+    secondary: false,
+    raised: false,
+    outlined: false,
+    disabled: false,
+    small: false,
+    large: false
 };
 
 export default DigitButtonGroup;

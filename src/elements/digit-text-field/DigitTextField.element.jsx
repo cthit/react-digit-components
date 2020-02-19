0eb6681903e8ca62a9143e1110b0ddd964a8f69f
Theodor Angergård <portalprogramming@gmail.com>
2020-02-19 01:34:19 +0100

    Default value to DigitTextField

--- src/elements/digit-text-field/DigitTextField.element.jsx
+++ src/elements/digit-text-field/DigitTextField.element.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 
 const DigitTextField = ({
-    value,
+    value = "",
     onChange,
     onBlur,
     upperLabel,

72cfdcd7882c9e681f15ee51dff9514e8423e99e
Theodor Angergård <portalprogramming@gmail.com>
2020-03-06 17:31:13 +0100

    Fix layout stories components

--- stories/layout/BackgroundColor.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { Fill } from "../../src/styles/digit-layout/DigitLayout.styles";
-
-const BackgroundColor = styled(Fill)`
-    background-color: ${props => props.color};
-`;
-
-export default BackgroundColor;
--- stories/layout/DummyItem.jsx
+++ stories/layout/DummyItem.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import { Center, Size } from "../../src/styles/digit-layout/DigitLayout.styles";
+import { Center } from "../../src/styles/digit-layout/DigitLayout.styles";
 import { Heading5 } from "../../src/styles/digit-text/DigitText.styles";
-import BackgroundColor from "./BackgroundColor";
 
 const DummyItem = ({ text, color }) => (
-    <div style={{ width: "100px", height: "100px" }}>
-        <BackgroundColor color={color}>
-            <Center>
-                <Heading5 text={text} />
-            </Center>
-        </BackgroundColor>
+    <div style={{ width: "100px", height: "100px", backgroundColor: color }}>
+        <Center>
+            <Heading5 text={text} />
+        </Center>
     </div>
 );
 

19cb7230853c92a7bded10b5df2ec164c23e2903
David Möller <davidm@student.chalmers.se>
2020-04-16 15:50:48 +0200

    Removed useEffect

--- src/elements/digit-text-area/DigitTextArea.element.jsx
+++ src/elements/digit-text-area/DigitTextArea.element.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { useEffect } from "react";
+import React from "react";
 import TextField from "@material-ui/core/TextField";
 import useLayoutMaterialUi from "../../styles/material-ui/use-layout-material-ui";
 

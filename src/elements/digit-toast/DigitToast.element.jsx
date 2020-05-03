bd4ada731a283768a5223b29b174c055d42be550
Theodor Angergård <portalprogramming@gmail.com>
2020-05-03 16:29:02 +0200

    Add close-able button for toast

--- src/elements/digit-toast/DigitToast.element.jsx
+++ src/elements/digit-toast/DigitToast.element.jsx
@@ -1,6 +1,9 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { StyledSnackbar, ToastButton } from "./DigitToast.element.styles";
+import CloseIcon from "@material-ui/icons/Close";
+import IconButton from "@material-ui/core/IconButton";
+import DigitIconButton from "../digit-icon-button";
 
 const DigitToast = ({
     open,
@@ -9,7 +12,8 @@ const DigitToast = ({
     duration,
     text,
     actionText,
-    actionHandler
+    actionHandler,
+    disableCloseable
 }) => {
     return (
         <StyledSnackbar
@@ -37,6 +41,13 @@ const DigitToast = ({
                             {actionText}
                         </ToastButton>
                     )}
+                    {!disableCloseable && (
+                        <DigitIconButton
+                            icon={CloseIcon}
+                            onClick={onClose}
+                            small
+                        />
+                    )}
                 </>
             }
         />
@@ -52,7 +63,9 @@ DigitToast.propTypes = {
     /** Callback function when you press the button on a toast */
     actionHandler: PropTypes.func,
     /** Button text */
-    actionText: PropTypes.string
+    actionText: PropTypes.string,
+    /** If true, then there's no close button */
+    disableCloseable: PropTypes.bool
 };
 
 export default DigitToast;

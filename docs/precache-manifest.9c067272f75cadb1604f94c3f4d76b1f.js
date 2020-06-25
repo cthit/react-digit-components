faee6fc22d2fced694d64255e0730a7ac45297a7
Theodor Angergård <portalprogramming@gmail.com>
2020-06-25 18:17:40 +0200

    Bumps version and rebuilds docs

---
+++ docs/precache-manifest.9c067272f75cadb1604f94c3f4d76b1f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4dbb226a4f4669d4ae250aa758010c",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.6c0e0a32687cccfbece4.bundle.js"
+  },
+  {
+    "url": "runtime~main.6c0e0a32687cccfbece4.bundle.js"
+  },
+  {
+    "url": "vendors~main.6c0e0a32687cccfbece4.bundle.js"
+  },
+  {
+    "url": "vendors~main.6c0e0a32687cccfbece4.bundle.js.LICENSE.txt"
+  }
+]);
